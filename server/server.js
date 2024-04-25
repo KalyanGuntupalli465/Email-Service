1df7425ce04f4af07442937f79786ac454ca3b09
Guntupalli Mohan kalyan <97778063+KalyanGuntupalli465@users.noreply.github.com>
2024-04-25 15:30:25 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,12 +6,6 @@ const app = express();
 dotenv.config();
 
 const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
-
 app.use(cors(
 
   {
